3c84c9c4e86aca35ea25d775da96fc433460b355
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2023-01-21 17:37:05 +0100

    Initial project structure

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'video-stream',
+  displayName: 'backend',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/video-stream',
+  coverageDirectory: '../../coverage/apps/backend',
 };
