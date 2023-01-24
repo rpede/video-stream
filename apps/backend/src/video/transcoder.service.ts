848f248f107b8431dd28dafc176643553daa2394
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2023-01-24 15:08:10 +0100

    Add writeToFile

--- apps/backend/src/video/transcoder.service.ts
+++ apps/backend/src/video/transcoder.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from "@nestjs/common";
 import * as ffmpeg from 'fluent-ffmpeg';
 import * as stream from 'stream';
 import { Format } from "./formats";
+import * as fs from 'fs';
 
 export type TranscoderOutput = {
     mimetype: string;
@@ -12,7 +13,7 @@ export type TranscoderOutput = {
 export class TranscoderService {
     private readonly logger = new Logger(TranscoderService.name);
 
-    stream(path: string, format: Format): TranscoderOutput {
+    stream(inputPath: string, format: Format): TranscoderOutput {
         this.logger.log(`Outputting: ${format.name}`);
         const transformStream = new stream.Transform({
             transform(chunk, encoding, callback) {
@@ -20,15 +21,22 @@ export class TranscoderService {
                 callback();
             },
         });
-        const proc = ffmpeg(path)
+        const proc = ffmpeg(inputPath)
             .preset(format.preset)
-            .on('end', () => {
-                this.logger.log('transcoding done');
-            })
-            .on('error', function (err) {
-                this.logger.error(err);
-            })
+            .on('end', () => this.logger.log('transcoding done'))
+            .on('error', (err) => this.logger.error(err))
             .pipe(transformStream, { end: true });
         return { mimetype: format.mimetype, stream: transformStream }
+    }
+
+    async writeToFile(inputPath: string, outputPath: string, format: Format) {
+        return new Promise((resolve, reject) => {
+            var writeStream = fs.createWriteStream(outputPath);
+            const proc = ffmpeg(inputPath)
+                .preset(format.preset)
+                .on('end', () => resolve(writeStream.bytesWritten))
+                .on('error', (err) => reject(err))
+                .pipe(writeStream, { end: true });
+        })
     }
 }
